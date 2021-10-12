33edb260d6d04d5ff9fe8424ed1f056abb8fb524
Scott Koranda <skoranda@gmail.com>
2021-10-12 07:54:19 -0500

    First attempt at instantiating plugin
    First attempt at adding code so that the plugin can be
    instantiated as required for COmanage Registry 4.0.0.

--- Model/CilogonEppnEnroller.php
+++ Model/CilogonEppnEnroller.php
@@ -9,4 +9,23 @@
 class CilogonEppnEnroller extends AppModel {
   // Required by COmanage Plugins
   public $cmPluginType = "enroller";
+
+  // Document foreign keys
+  public $cmPluginHasMany = array();
+
+  // Add behaviors
+  public $actsAs = array('Containable', 'Changelog' => array('priority' => 5));
+
+  // Association rules from this model to other models
+  public $belongsTo = array("CoEnrollmentFlowWedge");
+
+  // Validation rules for table elements
+  public $validate = array(
+    'co_enrollment_flow_wedge_id' => array(
+      'rule' => 'numeric',
+      'required' => true,
+      'allowEmpty' => false
+    )
+  );
+
 }
